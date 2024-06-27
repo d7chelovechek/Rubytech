37ce1d8a6a7fc87300ae586df64a1524ae717d15
Ayrat Khaydarov <info.chelovechek@gmail.com>
2024-06-28 01:07:26 +0300

    Implemented Network project for work with Rest client (wrapper of HttpClient).

---
+++ src/Rubytech.Network/Clients/RestClient.cs
@@ -0,0 +1,49 @@
+﻿using Rubytech.Abstractions.BaseObjects;
+using Rubytech.Network.Options;
+using System.Text.Json;
+
+namespace Rubytech.Network.Clients
+{
+    public class RestClient : BaseDisposable
+    {
+        private readonly HttpClient _client;
+
+        private readonly JsonSerializerOptions _serializationOptions;
+
+        public RestClient(RestClientOptions options)
+        {
+            _serializationOptions = options.SerializationOptions ?? JsonSerializerOptions.Default;
+
+            _client = new HttpClient()
+            {
+                BaseAddress = options.BaseUri
+            };
+
+            _client.DefaultRequestHeaders.Authorization = options.Authentication;
+        }
+
+        public async Task<TData?> GetAsync<TData>(
+            string endpoint, 
+            CancellationToken cancellationToken)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            using HttpResponseMessage response = await _client.SendAsync(request, cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+
+            string json = await response.Content.ReadAsStringAsync(cancellationToken);
+
+            return JsonSerializer.Deserialize<TData>(json, _serializationOptions);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (disposing)
+            {
+                _client?.Dispose();
+            }
+        }
+    }
+}
---
+++ src/Rubytech.Network/Options/RestClientOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace Rubytech.Network.Options
+{
+    public class RestClientOptions
+    {
+        public required Uri BaseUri { get; set; }
+        public required AuthenticationHeaderValue Authentication { get; set; }
+
+        public JsonSerializerOptions? SerializationOptions { get; set; }
+    }
+}
