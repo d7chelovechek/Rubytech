866a31b8a48792c236ec4bb70c8974b9f5923edb
Ayrat Khaydarov <info.chelovechek@gmail.com>
2024-06-28 01:01:38 +0300

    Implemented Abstractions project for store base implementation of disposable object.

---
+++ src/Rubytech.Abstractions/BaseObjects/BaseDisposable.cs
@@ -0,0 +1,28 @@
+﻿namespace Rubytech.Abstractions.BaseObjects
+{
+    public abstract class BaseDisposable : IDisposable
+    {
+        private bool _isDisposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            _isDisposed = true;
+        }
+
+        ~BaseDisposable()
+        {
+            Dispose(false);
+        }
+    }
+}
